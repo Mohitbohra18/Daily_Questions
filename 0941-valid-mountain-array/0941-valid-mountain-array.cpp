02f91c11abf333d6b49dc1be79ef05249cb346e9
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2025-08-18 16:15:29 +0530

    Time: 18 ms (69.54%), Space: 26.4 MB (8.45%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        
+        int maxele=*max_element(arr.begin(),arr.end());
+        auto it=find(arr.begin(),arr.end(),maxele);
+        int length=it-arr.begin();
+        int n=arr.size();
+        if(n<3||length==n-1||length==0) return false;
+        for(int i=0;i<length;i++){
+            if(arr[i]>=arr[i+1]) return false;
+        }
+        for(int i=length;i<n-1;i++){
+            if(arr[i]<=arr[i+1]) return false;
+        }
+
+        return true;
+    }
+};
