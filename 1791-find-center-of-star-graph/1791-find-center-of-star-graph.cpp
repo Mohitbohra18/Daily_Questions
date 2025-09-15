07ac0c9b044ad33fb16113285a17867a0c2afcaf
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2025-09-15 16:56:52 +0530

    Time: 63 ms (19.56%), Space: 127.4 MB (12.4%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int n = edges.size() + 1; // number of nodes
+        int n = edges.size() + 1; 
         vector<vector<int>> adj(n + 1);
         
-        // build adjacency list
+        
         for (auto &e : edges) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
@@ -13,10 +13,10 @@ public:
         vector<int> visited(n + 1, 0);
         vector<int> connections(n + 1, 0);
 
-        // Run DFS from node 1 (or any node)
+        
         dfs(1, visited, adj, connections);
 
-        // Find node with maximum degree
+        
         int center = 1;
         for (int i = 2; i <= n; i++) {
             if (connections[i] > connections[center]) {
