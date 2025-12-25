60d57f3e3c1701462c84a3d95652b4976cc43055
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2025-12-25 13:48:15 +0530

    Time: 3 ms (62.31%), Space: 8.3 MB (11.03%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    int f(int n,vector<int> &dp)
-    {
-     if(n<=1)return n;//the lowest possible value of the recursion parameter is the base case
+    // int f(int n,vector<int> &dp)
+    // {
+    //  if(n<=1)return n;//the lowest possible value of the recursion parameter is the base case
 
-     if(dp[n]!=-1)return dp[n];
+    //  if(dp[n]!=-1)return dp[n];
 
-     return dp[n]=  f(n-1,dp)+f(n-2,dp);
-    }
+    //  return dp[n]=  f(n-1,dp)+f(n-2,dp);
+    // }
 
 
     int fib(int n) {
         if(n<=1)
         return n;
 
-        vector<int> dp(n+1,-1);//recursion and memoization appraoch
-        return f(n,dp);
+        // vector<int> dp(n+1,-1);//recursion and memoization appraoch
+        // return f(n,dp);
+
+        vector<int> dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];//bottom up approach 
     }
 };
