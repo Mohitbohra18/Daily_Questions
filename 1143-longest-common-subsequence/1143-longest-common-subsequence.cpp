1a5df34cf9ee5c8c54280f5031cab067c925829d
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-01-02 00:20:27 +0530

    Time: 23 ms (80.62%), Space: 27.4 MB (51.14%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-      int lcsRec(string &s1,string &s2,int n,int m,vector<vector<int>> &dp)
-    {
-      if(n==0||m==0)// Base condition
-      return 0;
-      
-      if(dp[n][m]!=-1)
-      {
-          return dp[n][m];
-      }
-      
-      // Choice diagram based condn
-      
-      if(s1[n-1]==s2[m-1])
-      {
-          return dp[n][m]= 1+lcsRec(s1,s2,n-1,m-1,dp);
-      }
-      else
-      {
-          return dp[n][m]= max(lcsRec(s1,s2,n,m-1,dp),lcsRec(s1,s2,n-1,m,dp));
-      }
-      
-    }
-    int longestCommonSubsequence(string s1, string s2) {
-         int n=s1.size();
-        int m=s2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1)); // memoization 
+    int longestCommonSubsequence(string text1, string text2) {
+         int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0)); // top down approach
         
-        return lcsRec(s1,s2,n,m,dp);
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
