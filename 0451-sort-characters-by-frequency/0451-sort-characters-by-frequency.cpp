05a73ee21a44c80b3d3638c122b76a91c750d432
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-01-05 18:24:15 +0530

    Time: 3 ms (55.05%), Space: 11.2 MB (54.57%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
     string frequencySort(string s) {
-       unordered_map<char, int> freqMap;
-    for (char c : s) freqMap[c]++;
+    //    unordered_map<char, int> freqMap;
+    // for (char c : s) freqMap[c]++;
 
-    priority_queue<pair<int, char>> maxHeap;
-    for (auto &p : freqMap) {
-        maxHeap.push({p.second, p.first}); 
+    // priority_queue<pair<int, char>> maxHeap;
+    // for (auto &p : freqMap) {
+    //     maxHeap.push({p.second, p.first}); 
+    // }
+
+    // string result = "";
+    // while (!maxHeap.empty()) {
+    //     auto [freq, ch] = maxHeap.top();
+    //     maxHeap.pop();
+    //     result.append(freq, ch);
+    // }
+
+    // return result;
+
+
+vector<int> freq(256, 0);
+for(char ch : s)
+    freq[ch]++;
+
+vector<pair<char,int>> v;
+
+for(int i = 0; i < 256; i++) {
+    if(freq[i] > 0) {
+        v.push_back({char(i), freq[i]});
     }
+}
 
-    string result = "";
-    while (!maxHeap.empty()) {
-        auto [freq, ch] = maxHeap.top();
-        maxHeap.pop();
-        result.append(freq, ch);
-    }
+sort(v.begin(), v.end(), [](auto &a, auto &b) {
+    return a.second > b.second;
+});
 
-    return result;
+ string res="";
+ for(auto it:v)
+ {
+    res.append(it.second,it.first);
+ }
+return res;
     }
 };
