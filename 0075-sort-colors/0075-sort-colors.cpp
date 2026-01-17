73282c45ff9ca2c98ab6e6478d20fef85aea9adb
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-01-17 13:24:24 +0530

    Time: 0 ms (100%), Space: 11.7 MB (11.18%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,18 +2,66 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n=nums.size();
-        int low=0,mid=0,high=n-1;
-        while(mid<=high){
-            if(nums[mid]==0){
-                swap(nums[mid],nums[low]);
-                low++;
+        // int zero=0;
+        // int one=0;
+        // int two=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]==0)
+        //     {
+        //         zero++;
+        //     }
+        //     else if(nums[i]==1)
+        //     {
+        //         one++;
+        //     }
+        //     else
+        //     {
+        //         two++;
+        //     }
+        // }
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(zero>0)
+        //     {
+        //         nums[i]=0;
+        //         zero--;
+        //     }
+        //     else if(one!=0)
+        //    { nums[i]=1;
+        //     one--;
+        //     }
+        //     else 
+        //    { nums[i]=2;
+        //     two--;
+        //     }
+        // }
+
+        // Two Pointer Approach 
+
+        int i=0,j=n-1;
+        int mid=0;
+
+        while(mid<=j)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[mid],nums[i]);
+                i++;
                 mid++;
             }
-            else if(nums[mid]==2){
-                swap(nums[mid],nums[high]);
-                high--;
+            else if(nums[mid]==2)
+            {
+                swap(nums[mid],nums[j]);
+                j--;
             }
-            else mid++;
+            else
+            {
+
+            mid++;
+            }
         }
+        
+       
     }
 };
