3d70325f5100922eb55c512ccb7d0ffe9a082f06
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-01-17 13:52:09 +0530

    Time: 11 ms (47.52%), Space: 12.5 MB (38.48%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,53 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.length();
-        int maxLength = 0;
-        unordered_set<char> charSet;
-        int left = 0;
+        // LCS APPROACH
+        // int n = s.length();
+        // int maxLength = 0;
+        // unordered_set<char> charSet;
+        // int left = 0;
         
-        for (int right = 0; right < n; right++) {
-            if (charSet.count(s[right]) == 0) {
-                charSet.insert(s[right]);
-                maxLength = max(maxLength, right - left + 1);
-            } else {
-                while (charSet.count(s[right])) {
-                    charSet.erase(s[left]);
-                    left++;
+        // for (int right = 0; right < n; right++) {
+        //     if (charSet.count(s[right]) == 0) {
+        //         charSet.insert(s[right]);
+        //         maxLength = max(maxLength, right - left + 1);
+        //     } else {
+        //         while (charSet.count(s[right])) {
+        //             charSet.erase(s[left]);
+        //             left++;
+        //         }
+        //         charSet.insert(s[right]);
+        //     }
+        // }
+        
+        // return maxLength;
+
+
+        // SLIDING WINDOW APPROACH
+        
+        int n=s.size();
+        int low=0;
+        int high=0;
+        int mx=0;
+        unordered_map<char,int> m;
+
+        while(high<n)
+        {
+            m[s[high]]++;
+            while( m[s[high]]>1)
+            {
+                m[s[low]]--;
+                if(m[s[low]]==0)
+                {
+                    m.erase(s[low]);
                 }
-                charSet.insert(s[right]);
+                low++;
             }
+            int len=m.size();
+            mx=max(mx,len);
+            high++;
         }
+        return mx;
         
-        return maxLength;
     }
 };
