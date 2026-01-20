1cf80507ff5cff7476423b8f03d29b257974454d
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-01-21 01:14:48 +0530

    Time: 35 ms (87.39%), Space: 45.6 MB (40.92%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> subNum;
-        subNum[0] = 1;
-        int total = 0, count = 0;
+        // unordered_map<int, int> subNum;
+        // subNum[0] = 1;
+        // int total = 0, count = 0;
 
-        for (int n : nums) {
-            total += n;
+        // for (int n : nums) {
+        //     total += n;
 
-            if (subNum.find(total - k) != subNum.end()) {
-                count += subNum[total - k];
-            }
+        //     if (subNum.find(total - k) != subNum.end()) {
+        //         count += subNum[total - k];
+        //     }
 
-            subNum[total]++;
-        }
+        //     subNum[total]++;
+        // }
 
         // prefix summ
 
+   unordered_map<int, int> freq;
+        freq[0] = 1;   // prefix sum = 0 before starting
 
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            prefixSum += nums[i];
+
+            // check if (prefixSum - k) exists
+            if (freq.find(prefixSum - k) != freq.end()) {
+                count += freq[prefixSum - k];
+            }
+
+            // store current prefix sum
+            freq[prefixSum]++;
+        }
 
         return count;
     }
