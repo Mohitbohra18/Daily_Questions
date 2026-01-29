fe3c975d50e9f91aeafa9a3600eb706b622e287e
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-01-30 02:06:29 +0530

    Time: 0 ms (100%), Space: 69.5 MB (59.26%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -5,19 +5,36 @@ public:
         int low=0,high=0;
         int mx=0;
         int ones=0;
+        int zero=0;
 
+        // while(high<n)
+        // {
+        //     if(nums[high]==1)ones++;      
+            
+        //     int len=high-low+1;
+            
+        //     while(len-ones>k)
+        //     {
+        //         if(nums[low]==1)ones--;  
+        //         low++;
+        //         len=high-low+1;             
+        //     }
+
+        //     mx=max(mx,len);
+        //     high++;
+        // }
         while(high<n)
         {
-            if(nums[high]==1)ones++;      
+            if(nums[high]==0)zero++;      
             
-            int len=high-low+1;
             
-            while(len-ones>k)
+            
+            while(zero>k)
             {
-                if(nums[low]==1)ones--;  
-                low++;
-                len=high-low+1;             
+                if(nums[low]==0)zero--;  
+                low++;            
             }
+            int len=high-low+1;
 
             mx=max(mx,len);
             high++;
