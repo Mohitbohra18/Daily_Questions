927bd668ab6e1c7ca2813b1f51aeacae25666b21
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-03-18 17:23:55 +0530

    Time: 0 ms (100%), Space: 71.7 MB (80.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,15 +4,13 @@ public:
         int maxsum=INT_MIN;
         int sum=0;
 
-        for(int i=0;i<nums.size();++i)
-        {
-            sum+=nums[i];
-           maxsum=max(sum,maxsum);
-            if(sum<0)
-            {
-                sum=0;
-            }
-        }
+       for(int i=0;i<nums.size();i++)
+       {
+        sum+=nums[i];
+        maxsum=max(maxsum,sum);
+        if(sum<0)
+        sum=0;
+       }
         return maxsum;
     }
 };
