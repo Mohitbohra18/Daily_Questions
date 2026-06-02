127540acfc934afa1dd86471691475c8c9abac6b
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-06-02 05:33:12 +0530

    Time: 2 ms (15.92%), Space: 9.8 MB (10.44%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -3,16 +3,19 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>ans(numRows);
         ans[0]={1};
-        if(numRows==1) return ans;
-        for(int i=1;i<numRows;i++){
-            vector<int>list(i+1);
-            list[0]=1;
-            list[i]=1;
-            for(int j=1;j<i;j++){
-                list[j]=ans[i-1][j-1]+ans[i-1][j];
+        if(numRows==1)return ans;
+        for(int i=1;i<numRows;i++)
+        {
+            vector<int> row(i+1);
+            row[0]=1;
+            row[i]=1;
+            for(int j=1;j<i;j++)
+            {
+                row[j]=ans[i-1][j-1]+ans[i-1][j];
             }
-            ans[i]=list;
+            ans[i]=row;
         }
         return ans;
+
     }
 };
