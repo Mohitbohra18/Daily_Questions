33579815f20130d29e5ce3fe2e5fdfa057577551
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-06-17 13:43:53 +0530

    Time: 50 ms (9.2%), Space: 23.9 MB (67.82%) - LeetHub

--- 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
@@ -1,56 +1,39 @@
 class Solution {
 public:
     char processStr(string s, long long k) {
-        const long long LIM = 4e18;
-
-        vector<long long> lenAfter(s.size());
-
         long long len = 0;
-
-        for (int i = 0; i < s.size(); i++) {
-            char c = s[i];
-
-            if ('a' <= c && c <= 'z') {
+        for (char c : s) {
+            if (islower(c)) {
                 len++;
             }
             else if (c == '*') {
                 if (len > 0) len--;
             }
             else if (c == '#') {
-                len = min(LIM, len * 2);
+                len *= 2;
             }
-            // '%' doesn't change length
-
-            lenAfter[i] = len;
+            else if (c == '%') {}
         }
-
         if (k >= len) return '.';
-
-        for (int i = (int)s.size() - 1; i >= 0; i--) {
+        for (int i = s.size() - 1; i >= 0; i--) {
             char c = s[i];
-            long long curLen = lenAfter[i];
-
-            if ('a' <= c && c <= 'z') {
-                if (k == curLen - 1)
+            if (islower(c)) {
+                if (k == len - 1) {
                     return c;
-
-                len = curLen - 1;
-            }
-            else if (c == '#') {
-                long long prevLen = curLen / 2;
-                k %= prevLen;
-                len = prevLen;
-            }
-            else if (c == '%') {
-                k = curLen - 1 - k;
-                len = curLen;
+                }
+                len--;
             }
             else if (c == '*') {
-                long long prevLen = curLen + 1;
-                len = prevLen;
+                len++;
+            }
+            else if (c == '#') {
+                len /= 2;
+                k %= len;
+            }
+            else if (c == '%') {
+                k = len - 1 - k;
             }
         }
-
         return '.';
     }
 };
