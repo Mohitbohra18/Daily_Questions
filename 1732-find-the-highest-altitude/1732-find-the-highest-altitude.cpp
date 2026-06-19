38bb79aa4722dd322650d1a57f075fd93a5281c2
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-06-19 09:07:37 +0530

    Time: 0 ms (100%), Space: 10.8 MB (44.68%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int mx=0;
+        int sum=0;
+
+        for(int i=0;i<gain.size();i++)
+        {
+            sum+=gain[i];
+            mx=max(mx,sum);
+            
+        }
+        return mx;
+        
+    }
+};
