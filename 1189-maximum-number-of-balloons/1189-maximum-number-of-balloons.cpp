faa68586151c1e0c6795353f64c1c23f78c286cc
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-06-22 15:41:39 +0530

    Time: 3 ms (12.33%), Space: 9.1 MB (15.37%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -3,12 +3,12 @@ public:
     int maxNumberOfBalloons(string text) {
         unordered_map<char,int> umap;
 
-        // Step 1: Count frequency of each character
+        
         for(char ch : text){
             umap[ch]++;
         }
 
-        // Step 2: Calculate the minimum possible balloons
+        
         int result = INT_MAX;
 
         result = min(result, umap['b']);
