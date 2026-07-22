0661d046d7797d382648e263d0387c69a2d1fb48
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-07-22 16:42:06 +0530

    Time: 3 ms (65.62%), Space: 8.3 MB (11.27%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n==0||n==1)return n;
+    vector<int> dp;
 
-        return fib(n-1)+fib(n-2);
-        
+    int fib(int n) {
+        dp.assign(n + 1, -1);
+        return solve(n);
+    }
+
+    int solve(int n) {
+        if (n <= 1) return n;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        return dp[n] = solve(n - 1) + solve(n - 2);
     }
 };
