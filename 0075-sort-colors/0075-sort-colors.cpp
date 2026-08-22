f6690fcbde31ffb0e1d0dadcd9bfa8194be9e3a4
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-08-22 22:09:26 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.52%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,65 +2,29 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n=nums.size();
-        // int zero=0;
-        // int one=0;
-        // int two=0;
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(nums[i]==0)
-        //     {
-        //         zero++;
-        //     }
-        //     else if(nums[i]==1)
-        //     {
-        //         one++;
-        //     }
-        //     else
-        //     {
-        //         two++;
-        //     }
-        // }
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(zero>0)
-        //     {
-        //         nums[i]=0;
-        //         zero--;
-        //     }
-        //     else if(one!=0)
-        //    { nums[i]=1;
-        //     one--;
-        //     }
-        //     else 
-        //    { nums[i]=2;
-        //     two--;
-        //     }
-        // }
+        int zero=0;
+        int two=n-1;
+        int one=0;
 
-        // Two Pointer Approach 
-
-        int i=0,j=n-1;
-        int mid=0;
-
-        while(mid<=j)
+        while(one<=two)
         {
-            if(nums[mid]==0)
+            if(nums[one]==0)
             {
-                swap(nums[mid],nums[i]);
-                i++;
-                mid++;
+                swap(nums[zero],nums[one]);
+                zero++;
+                one++;
+                
             }
-            else if(nums[mid]==2)
+            else if(nums[one]==1)
             {
-                swap(nums[mid],nums[j]);
-                j--;
+                one++;
             }
             else
             {
-            mid++;
+               swap(nums[two],nums[one]);
+                two--;
             }
         }
         
-       
     }
 };
