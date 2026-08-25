508d9c9321bfadb3d7a02c67c14d1ecaa02fe1ff
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-08-25 15:30:32 +0530

    Time: 3 ms (15.64%), Space: 9.5 MB (17.81%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<char, int> charCount;
-
-        for (char c : s) {
-            charCount[c]++;
+        int oddCount = 0;
+        unordered_map<char, int> ump;
+        for(char ch : s) {
+            ump[ch]++;
+            if (ump[ch] % 2 == 1)
+                oddCount++;
+            else    
+                oddCount--;
         }
-
-        int length = 0;
-        bool hasOdd = false;
-
-        for (auto &pair : charCount) {
-            int count = pair.second;
-            if (count % 2 == 0) {
-                length += count;
-            } else {
-                length += count - 1;
-                hasOdd = true;
-            }
-        }
-
-        if (hasOdd) {
-            length += 1;
-        }
-
-        return length;     
+        if (oddCount > 1)
+            return s.length() - oddCount + 1;
+        return s.length();
     }
 };
