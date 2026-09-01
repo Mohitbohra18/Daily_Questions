84ed1200d9d56958bf63f1f191cd344ca56a9595
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-09-01 20:41:36 +0530

    Time: 0 ms (100%), Space: 13.3 MB (94.02%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,51 +1,12 @@
 class Solution {
 public:
-    string removeDuplicates(string s) {
-        // stack<char> st;
-        // st.push(s[0]);
-        // string str;
-        
-
-        // for(int i=1;i<s.size();i++)
-        // {
-           
-        //    if(!st.empty()&&(st.top()==s[i]) )
-        //    {
-        //     st.pop();
-        //    }
-        //    else
-        //    {
-        //     st.push(s[i]);
-        //    }
-        // }
-        
-        // int ind=0;
-        // while(!st.empty())
-        // {
-        //     str.push_back(st.top());
-        //     st.pop();
-        //     ind++;          
-        // }
-        // reverse(str.begin(),str.end());
-        // return str;
-
-        //METHOD 2
-
-        string res="";
-
-        for(int i=0;i<s.size();i++)
-        {
-            if(!res.empty()&&res.back()==s[i])
-            {
-                res.pop_back();
-
-            }
-            else
-            {
-                res.push_back(s[i]);
-            }
+        string removeDuplicates(string s) {
+        int i = 0, n = s.length();
+        for (int j = 0; j < n; ++j, ++i) {
+            s[i] = s[j];
+            if (i > 0 && s[i - 1] == s[i]) // count = 2
+                i -= 2;
         }
-        return res;
+        return s.substr(0, i);
     }
 };
-
