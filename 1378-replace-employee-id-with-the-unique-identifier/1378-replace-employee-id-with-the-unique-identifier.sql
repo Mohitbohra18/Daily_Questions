eb21da377a4e13dc358321afb43558c349386452
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-09-02 20:28:53 +0530

    Time: 1083 ms (97.01%), Space: 0B (100%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,5 +1,5 @@
-/* Write your PL/SQL query statement below */
-select eu.unique_id , e.name 
-from Employees e
-left join EmployeeUNI eu
-on e.id=eu.id;
+# Write your MySQL query statement below
+select e2.unique_id,e1.name
+from Employees e1
+left join EmployeeUNI e2
+on e1.id=e2.id;
