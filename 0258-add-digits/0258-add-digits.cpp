dff69038440207a3fcf51482f92a9fae5d094388
Mohit Bohra <117972330+Mohitbohra18@users.noreply.github.com>
2026-09-08 23:41:20 +0530

    Time: 0 ms (100%), Space: 8.6 MB (45.51%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -2,19 +2,23 @@ class Solution {
 public:
     int addDigits(int num) {
         
-        while(num>9)
-        {
-            int n=num;
-            int sum =0;
-            while(n!=0)
-            {
-                int rem=n%10;
-                sum+=rem;
-                n=n/10;
-            }
-            num=sum;
-        }
+        // while(num>9)
+        // {
+        //     int n=num;
+        //     int sum =0;
+        //     while(n!=0)
+        //     {
+        //         int rem=n%10;
+        //         sum+=rem;
+        //         n=n/10;
+        //     }
+        //     num=sum;
+        // }
 
-        return num;
+        if(num==0)return 0;
+
+        return 1+(num -1)%9;
+
+        // return num;
     }
 };
